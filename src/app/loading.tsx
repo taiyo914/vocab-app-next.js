be78a79cc5765e56724c3c70659b2e1bacf54f75
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-05 02:25:37 +0900

    軽微な修正

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Loading () {
   return (
-    <div className="flex justify-center" aria-label="読み込み中">
+    <div className="flex justify-center items-center h-screen" aria-label="読み込み中">
       <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div>
     </div>
   );
--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -227,7 +227,6 @@ export default function Settings() {
             </div>
           </div>
 
-          {/* Submit ボタン */}
           <button
             type="submit"
             className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600 mr-1"
