b7f699868a0e07828a6e706900ab8f9176af08c9
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-05 09:40:06 +0900

    Changed route name "/login" to "/signin"

--- src/components/LogOutBtn.tsx
+++ src/components/LogOutBtn.tsx
@@ -7,7 +7,7 @@ export default function LogOutBtn() {
   const router = useRouter()
   const singOut = async () =>{
     const  { error } = await supabase.auth.signOut()
-    router.push("/login")
+    router.push("/signin")
   }
   return (
       <button onClick={singOut} className="py-1 px-2 bg-gray-300 rounded-lg"> Log out</button>
