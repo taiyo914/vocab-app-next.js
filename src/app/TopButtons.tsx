275343d0357ec6d18b8eb060bbf26026b336d7d8
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-17 15:44:33 +0900

    Fixed xs prefixer

--- src/app/TopButtons.tsx
+++ src/app/TopButtons.tsx
@@ -7,19 +7,18 @@ import { PlayCircleIcon } from "@heroicons/react/16/solid";
 const TopButtons = () => {
   return (
     <div> {/* Homeコンポーネントのflex-colの影響で、 これがないと画面幅に広がらないので消さないでください*/}
-      <div className="p-3 xs:p-5 mx-auto max-w-[2000px]">
-        <div className="flex jusity-center flex-col xs:flex-row xs:space-x-4 xs:my-0 my-2">
+      <div className="px-3 py-5 xs:py-4 mx-auto max-w-[2000px]">
+        <div className="flex jusity-center xs:flex-col gap-x-3 xs:gap-y-3 my-0">
           <Link
             href="new"
             className="
               text-center font-semibold
-              w-full xs:w-1/2 py-2   
-              xs:mb-0 mb-1
+              w-full py-2
               border rounded-md shadow-md 
               hover:bg-gray-200  transition-all duration-300 
-              flex items-center justify-center space-x-1"
+              flex items-center justify-center gap-1 xs:gap-0"
           >
-            <PlusCircleIcon className="h-7"/>
+            <PlusCircleIcon className="h-6"/>
             <div>単語を追加する</div>
             <div className="invisible "></div>
           </Link>
@@ -27,22 +26,18 @@ const TopButtons = () => {
             href="review"
             className="
               text-center font-semibold 
-              w-full xs:w-1/2 py-2 
-              mt-2 xs:mt-0
+              w-full py-2 
               bg-blue-500 text-white
               rounded-md shadow-md 
               hover:bg-blue-600 transition-all duration-300
-              flex items-center justify-center space-x-1"
+              flex items-center justify-center gap-1 xs:gap-0.5"
           > 
-            <PlayCircleIcon className="h-7"/>
+            <PlayCircleIcon className="h-6"/>
             <div>ページを復習</div>
             <div className="invisible w-2"></div>
           </Link>
         </div>
       </div>
-      {/* <div className="flex justify-end mr-5 ">
-        <Link href={"/experiment"} className="p-2 border rounded-full">experiment</Link>
-      </div> */}
     </div>
   );
 };
