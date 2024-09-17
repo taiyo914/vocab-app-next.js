d4da0ad05d2df26afb011c70b4bd6c4381371e65
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-17 20:02:07 +0900

    Changed wit xs

--- src/app/display/displayContent/CardsDisplay.tsx
+++ src/app/display/displayContent/CardsDisplay.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 const CardsDisplay = ({ words }: { words: WordType[] }) => {
   return (
     <motion.div
-      className="grid xs:gap-4 gap-2 px-4 py-2"
+      className="grid gap-4 xs:gap-2 px-4 py-2"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.7 }}
