fd8e13ec019c6dd936e8122098663fcff07a4f45
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-17 19:11:19 +0900

    Changed with xs

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -3,22 +3,23 @@ import { FaFacebook, FaTwitter, FaInstagram,  } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer className="bg-gray-100 border-t">
-      <div className="
-        flex items-center justify-between
-        xs:flex-row xs:space-x-4 flex xs:space-y-0
-        flex-col space-y-3 
-        xs:py-8 px-5 py-6">
-        <p className="text-gray-400 xs:text-base text-sm">&copy; Created by Taiyo Suzuki</p>
-        <div className="flex items-center justify-end space-x-4" >
-          <p className="text-sm text-gray-400">Share on</p>
+      <div 
+        className="
+          flex items-center justify-between
+          xs:flex-col xs:gap-y-2
+          px-6 xs:px-5 py-6 xs:py-4"
+      >
+        <p className="text-gray-400 xs:text-sm">&copy; Created by Taiyo Suzuki</p>
+        <div className="flex items-center justify-end space-x-3" >
+          <p className="xs:text-sm text-gray-400">Share on</p>
           <a href="https://facebook.com" aria-label="Facebook">
-            <FaFacebook size={24} />
+            <FaFacebook />
           </a>
           <a href="https://twitter.com" aria-label="Twitter">
-            <FaTwitter size={24} />
+            <FaTwitter  />
           </a>
           <a href="https://instagram.com" aria-label="Instagram">
-            <FaInstagram size={24} />
+            <FaInstagram  />
           </a>
         </div>
       </div>
