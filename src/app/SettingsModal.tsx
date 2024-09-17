72b05c51edc783225c60659172961c9a1f0f1adf
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-17 18:44:20 +0900

    Minor change

--- src/app/SettingsModal.tsx
+++ src/app/SettingsModal.tsx
@@ -5,6 +5,10 @@ import useOpenModalStore from "@/store/openModalStore";
 import useUserStore from "@/store/userStore";
 import { WordsSettingsType } from "@/types/Types";
 import { motion, AnimatePresence } from "framer-motion";
+import { BsEraser } from "react-icons/bs";
+import { IoSettingsOutline } from "react-icons/io5";
+import { HiXMark } from "react-icons/hi2";
+import { FaRegCircleXmark } from "react-icons/fa6";
 
 export default function SettingsModal() {
   const { isOpen, toggleModal } = useOpenModalStore();
@@ -37,6 +41,18 @@ export default function SettingsModal() {
     //page_offset以外
   ]);
 
+  //後ろの画面をスクロールできなくする設定
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, [isOpen]);
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { name, value } = e.target;
     
@@ -67,6 +83,21 @@ export default function SettingsModal() {
     }
   };
 
+  //数字を入力するフィールドにテキストを入力させない関数
+  const handleInput = (e:any) => {
+    const value = e.target.value;
+    e.target.value = value.replace(/\D/, ''); 
+  };
+
+  // 日付をクリアする関数を作成（iPhoneだと一度入力した日付がリセットできないため）
+  const clearDates = () => {
+    setTemporarySettings((prevSettings) => ({
+      ...prevSettings,
+      start_date: null,
+      end_date: null
+    }) as WordsSettingsType );
+  };
+
   return (
     <AnimatePresence>
       {isOpen && (
@@ -90,42 +121,45 @@ export default function SettingsModal() {
             className="
               border shadow-lg rounded-2xl
               z-50
-              xs:m-7 m-5 
-              w-1/2 max-w-2xl xs:min-w-[400px] min-w-[300px] 
+              w-1/2 max-w-2xl min-w-[340px] 
               overflow-hidden
-              max-h-[90vh]
+              max-h-[75vh]
               relative"
           >
-            <div className="bg-white p-5 xs:px-10 xs:py-7 max-h-[90vh] overflow-y-auto " >
-              <h1 className="text-2xl font-bold mb-5 text-center">設定</h1>
+            <div className="bg-white px-10 xs:px-7 py-7 max-h-[75vh] overflow-y-auto " >
+              <h1 className="text-2xl font-bold mb-3 text-center">設定</h1>
               <form onSubmit={handleSubmit} className="grid grid-cols-1 ">
                 <div className="mb-4">
-                  <label className="block font-medium text-gray-500 xs:mb-1 xs:-mt-2 -mt-4 ">表示件数</label>
+                  <label className="block font-medium mb-1 ml-0.5 text-gray-600">表示件数</label>
                   <input
                     type="number"
                     name="display_count"
                     min = {1}
                     value={temporarySettings?.display_count}
                     onChange={handleChange}
+                    onInput={handleInput}
+                    required
                     className="
-                      xs:py-2 py-1 px-3 w-full 
-                      font-[550] 
-                      border border-gray-300 rounded-lg 
+                      py-2 px-3 w-full 
+                      border border-gray-300 rounded-lg
+                      bg-gray-50
+                      font-[500] xs:font-semibold 
                       focus:outline-none focus:border-black"
                   />
                 </div>
                 <div className="mb-7">
-                  <label className="block font-medium xs:mb-1  text-gray-500">並び替え</label>
+                  <label className="block font-medium mb-1 ml-0.5 text-gray-600">並び替え</label>
                   <div className="flex items-center space-x-2">
                     <select
                       name="sort_field"
                       value={temporarySettings?.sort_field}
                       onChange={handleChange}
                       className="
-                        xs:pl-3 pl-2 xs:py-3 py-2 pr-0 
-                        border border-gray-300 rounded-lg 
-                        w-full font-[580]
-                        focus:outline-none focus:border-black"
+                        appearance-none w-full bg-gray-50
+                        border border-gray-300 rounded-lg
+                        pl-3 py-2 
+                        font-[580] xs:font-semibold
+                        focus:outline-none focus:border-black "
                     >
                       <option value="increment">作成日</option>
                       <option value="index">優先度</option>
@@ -140,10 +174,11 @@ export default function SettingsModal() {
                       value={temporarySettings?.sort_order}
                       onChange={handleChange}
                       className="
-                        xs:pl-3 pl-2 xs:py-3 py-2 pr-0 
-                        border border-gray-300 rounded-lg 
-                        w-full font-[580]
-                        focus:outline-none focus:border-black"
+                        appearance-none w-full bg-gray-50
+                        border border-gray-300 rounded-lg
+                        pl-3 py-2 
+                        font-[580] xs:font-semibold
+                        focus:outline-none focus:border-black "
                     >
                       <option value="ASC">昇順</option>
                       <option value="DESC">降順</option>
@@ -157,10 +192,12 @@ export default function SettingsModal() {
                   <div className="flex justify-center text-sm">
                     <button
                       type="button"
-                      className="bg-gray-200 text-gray-700 px-5 py-2 text-center rounded-full"
+                      className="bg-gray-200 text-gray-700 w-full py-2 text-center rounded-full"
                       onClick={toggleDetails}
                     >
-                      {showDetails ? "閉じる" : "詳細設定"}
+                      {showDetails 
+                        ? (<div className="flex items-center justify-center gap-1"><FaRegCircleXmark />詳細設定を閉じる</div>) 
+                        : (<div className="flex items-center justify-center gap-1"><IoSettingsOutline />詳細設定を開く</div>)}
                     </button>
                   </div>
 
@@ -174,8 +211,8 @@ export default function SettingsModal() {
                         transition={{ duration: 0.5 }}
                         className="overflow-hidden"
                       >
-                        <div className="mb-4 mt-1">
-                          <label className="block font-medium xs:mb-1  text-gray-500">優先度</label>
+                        <div className="my-4">
+                          <label className="block font-medium mb-1 ml-0.5 text-gray-600">優先度</label>
                           <div className="flex items-center space-x-2 ">
                             <input
                               type="number"
@@ -184,11 +221,14 @@ export default function SettingsModal() {
                               max={temporarySettings?.end_index}
                               value={temporarySettings?.start_index}
                               onChange={handleChange}
+                              onInput={handleInput}
+                              required
                               className="
-                              xs:py-2 py-1 px-3
-                              w-full font-[550] 
-                              border border-gray-300 rounded-lg  
-                              focus:outline-none focus:border-black"
+                                py-2 px-3 w-full 
+                                border border-gray-300 rounded-lg
+                                bg-gray-50
+                                font-[500] xs:font-semibold 
+                                focus:outline-none focus:border-black"
                             />
                             <span className=" text-gray-600 min-w-max ">から</span>
                             <input
@@ -198,10 +238,13 @@ export default function SettingsModal() {
                               max={10}
                               value={temporarySettings?.end_index}
                               onChange={handleChange}
+                              onInput={handleInput}
+                              required
                               className="
-                                xs:py-2 py-1 px-3
-                                w-full font-[550] 
-                                border border-gray-300 rounded-lg  
+                                py-2 px-3 w-full 
+                                border border-gray-300 rounded-lg
+                                bg-gray-50
+                                font-[500] xs:font-semibold 
                                 focus:outline-none focus:border-black"
                             />
                             <span className=" text-gray-600 min-w-max">まで</span>
@@ -209,7 +252,7 @@ export default function SettingsModal() {
                         </div>
 
                         <div className="mb-4 ">
-                          <label className="block font-medium xs:mb-1 text-gray-500">復習回数</label>
+                          <label className="block font-medium mb-1 ml-0.5 text-gray-600">復習回数</label>
                           <div className="flex items-center space-x-2">
                             <input
                               type="number"
@@ -218,10 +261,13 @@ export default function SettingsModal() {
                               max={temporarySettings?.end_review_count}
                               value={temporarySettings?.start_review_count}
                               onChange={handleChange}
+                              onInput={handleInput}
+                              required
                               className="
-                                xs:py-2 py-1 px-3
-                                w-full font-[550] 
-                                border border-gray-300 rounded-lg  
+                                py-2 px-3 w-full 
+                                border border-gray-300 rounded-lg
+                                bg-gray-50
+                                font-[500] xs:font-semibold 
                                 focus:outline-none focus:border-black"
                             />
                             <span className="min-w-max  text-gray-600">から</span>
@@ -232,10 +278,13 @@ export default function SettingsModal() {
                               max={100}
                               value={temporarySettings?.end_review_count}
                               onChange={handleChange}
+                              onInput={handleInput}
+                              required
                               className="
-                                xs:py-2 py-1 px-3
-                                w-full font-[550] 
-                                border border-gray-300 rounded-lg  
+                                py-2 px-3 w-full 
+                                border border-gray-300 rounded-lg
+                                bg-gray-50
+                                font-[500] xs:font-semibold 
                                 focus:outline-none focus:border-black"
                             />
                             <span className="min-w-max text-gray-600 text-center">まで</span>
@@ -243,7 +292,15 @@ export default function SettingsModal() {
                         </div>
 
                         <div className="">
-                          <label className="block font-medium xs:mb-1 text-gray-500">日付範囲</label>
+                          <div className="flex justify-between items-center mb-1 ml-0.5">
+                            <label className="block font-medium  text-gray-600 ">日付範囲</label>
+                            <div onClick={clearDates} 
+                                className="text-gray-500 text-sm flex items-center justify-end cursor-pointer"
+                              >
+                                  <BsEraser />
+                                  日付をクリア
+                            </div>
+                          </div>
                           <div className="flex flex-wrap items-baseline -mr-2">
                             <div className="flex flex-grow items-center mb-2">
                               <select
@@ -251,11 +308,11 @@ export default function SettingsModal() {
                                 value={temporarySettings?.date_field}
                                 onChange={handleChange}
                                 className="
-                                  xs:p-3 p-2 
-                                  font-[580] 
-                                  border border-gray-300 rounded-lg 
-                                  focus:outline-none focus:border-black 
-                                  flex-grow  max-w"
+                                  appearance-none w-full bg-gray-50
+                                  border border-gray-300 rounded-lg
+                                  pl-3 py-2 
+                                  font-[580] xs:font-semibold
+                                  focus:outline-none focus:border-black "
                               >
                                 <option value="created_at">作成日</option>
                                 <option value="updated_at">更新日</option>
@@ -271,11 +328,11 @@ export default function SettingsModal() {
                                   value={temporarySettings?.start_date ?? ""}
                                   onChange={handleChange}
                                   className="
-                                  qxs:p-3 p-2 
-                                  font-[580] 
-                                  border border-gray-300 rounded-lg f
-                                  ocus:outline-none focus:border-black 
-                                  flex-grow"
+                                    flex-grow bg-gray-50 h-11
+                                    border border-gray-300 rounded-lg
+                                    px-3  
+                                    xs:font-semibold
+                                    focus:outline-none focus:border-black "
                                 />
                                 <span className="text-gray-600 min-w-8 mx-2 ">から</span>
                               </div>
@@ -286,12 +343,12 @@ export default function SettingsModal() {
                                   value={temporarySettings?.end_date ?? ""}
                                   onChange={handleChange}
                                   className="
-                                    xs:p-3 p-2 
-                                    font-[580] 
-                                    border border-gray-300 rounded-lg 
-                                    focus:outline-none focus:border-black 
-                                    flex-grow"
-                                />
+                                    flex-grow bg-gray-50 h-11
+                                    border border-gray-300 rounded-lg
+                                    px-3
+                                    xs:font-semibold
+                                    focus:outline-none focus:border-black "
+                                  />
                                 <span className="text-gray-600 min-w-8 mx-2">まで</span>
                               </div>
                             </div>
@@ -303,12 +360,16 @@ export default function SettingsModal() {
                 <div className="flex justify-between space-x-3 mt-7">
                   <button
                     type="submit"
-                    className="bg-blue-500 text-lg text-white px-3 xs:py-3 py-2 rounded-lg hover:bg-blue-600 transition-all w-full"
+                    className="
+                      bg-blue-500 text-lg text-white font-[550]
+                      px-3 py-3 rounded-lg hover:bg-blue-600 transition-all w-full"
                   >
                     設定を保存
                   </button>
                   <div
-                    className="bg-gray-500 text-lg text-white px-3 xs:py-3 py-2 rounded-lg hover:bg-gray-600 transition-all mr-1 w-full text-center"
+                    className="
+                      bg-gray-500 text-lg text-white font-[550]
+                      px-3 py-3 rounded-lg hover:bg-gray-600 transition-all w-full text-center cursor-pointer"
                     onClick={onClose}
                   >
                     閉じる
