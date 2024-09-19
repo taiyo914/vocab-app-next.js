373ab202a3bd7512bda87b6b19a767b9e7d2ed86
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-19 14:36:19 +0900

    Created wordSpeaker

--- src/app/experiment/page.tsx
+++ src/app/experiment/page.tsx
@@ -1,19 +1,22 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import CustomSlider from "./CustomSlider";
 import DndKit from "./DndKit";
 // import DND from './react-beautiful-dnd'
 import Exercise from "./Exercise";
 import TaskManager from "./TaskManager";
 import ReviewSettingsModal from "../review/ReviewSettingsModal";
+import WordSpeaker from "./WordSpeaker";
 
 export default function page() {
   return (
     <>
+      <WordSpeaker/>
       {/* <CustomSlider/> */}
       {/* <DND/> */}
-      <DndKit />
+      {/* <DndKit /> */}
       {/* <Exercise/> */}
-      <TaskManager />
+      {/* <TaskManager /> */}
       {/* <ReviewSettingsModal/> */}
     </>
   );
