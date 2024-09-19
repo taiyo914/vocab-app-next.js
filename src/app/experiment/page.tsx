7228e372e165c8779a32701e90c7ef14e69f657b
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-19 19:02:14 +0900

    Nothing changed

--- src/app/experiment/page.tsx
+++ src/app/experiment/page.tsx
@@ -7,7 +7,7 @@ import Exercise from "./Exercise";
 import TaskManager from "./TaskManager";
 import ReviewSettingsModal from "../review/ReviewSettingsModal";
 import WordSpeaker from "./WordSpeaker";
-import SpeechButton from "@/components/SpeechBotton";
+import SpeechButton from "@/components/SpeechButton";
 
 export default function page() {
   return (
