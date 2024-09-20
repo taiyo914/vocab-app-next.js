f22c79608285a45556bad93a5eb350fc8741b4ec
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-21 01:00:41 +0900

    Implement delete button

--- src/components/EditWordModal.tsx
+++ src/components/EditWordModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState}from "react";
 import { createClient } from "@/utils/supabase/client";
 import CustomSlider from "./CustomSlider";
 import useUserStore from "@/store/userStore";
@@ -21,6 +21,7 @@ const EditWordModal: React.FC<EditWordModalProps> = ({
 }) => {
   const supabase = createClient();
   const { words, setWords, fetchWords } = useUserStore();
+  const [isDeleteConfirmOpen, setIsDeleteConfirmOpen] = useState(false); 
 
   const handleSaveChanges = async () => {
     try {
@@ -60,7 +61,7 @@ const EditWordModal: React.FC<EditWordModalProps> = ({
 
   return (
    <Modal isOpen ={isOpen} onClose={onClose}>
-      <div className="flex items-center justify-center mt-2 mb-3">
+      <div className="relative flex items-center justify-center mt-2 mb-3">
         <PencilSquareIcon className="h-5" />
         <h2 className="font-bold text-xl ">カードを編集</h2>
       </div>
@@ -168,6 +169,40 @@ const EditWordModal: React.FC<EditWordModalProps> = ({
           キャンセル
         </button>
       </div>
+
+      <div className="flex justify-center mt-4">
+        <button
+          onClick={() => setIsDeleteConfirmOpen(true)} 
+          className="w-1/2 py-2 bg-red-500 hover:bg-red-600 text-white font-[580] rounded-lg transition duration-300"
+        >
+          単語を削除する
+        </button>
+      </div>
+
+      {isDeleteConfirmOpen && (
+          <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
+            <div className="bg-white p-6 rounded-lg shadow-lg">
+              <h3 className="text-lg font-bold mb-4">本当に削除しますか？</h3>
+              <div className="flex justify-center space-x-4">
+                <button
+                  onClick={() => {
+                    handleDelete();
+                    setIsDeleteConfirmOpen(false);
+                  }}
+                  className="px-4 py-2 bg-red-500 text-white rounded-lg"
+                >
+                  はい、削除します
+                </button>
+                <button
+                  onClick={() => setIsDeleteConfirmOpen(false)} // キャンセル
+                  className="px-4 py-2 bg-gray-300 text-black rounded-lg"
+                >
+                  キャンセル
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
   </Modal>
   );
 };
