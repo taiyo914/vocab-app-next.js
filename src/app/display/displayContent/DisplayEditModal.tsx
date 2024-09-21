98d8bab689e8871d3914e8d01aee06b8c62f49fa
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-21 12:54:38 +0900

    Changed layout

--- src/app/display/displayContent/DisplayEditModal.tsx
+++ src/app/display/displayContent/DisplayEditModal.tsx
@@ -1,11 +1,13 @@
-import React, {useState}from "react";
+import React, { useState } from "react";
 import { createClient } from "@/utils/supabase/client";
-import CustomSlider from "../../../components/CustomSlider";
+import CustomSlider from "@/components/CustomSlider";
 import useUserStore from "@/store/userStore";
 import { WordType } from "@/types/Types";
-import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { ChevronDownIcon, ChevronRightIcon, PencilSquareIcon, TrashIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import Modal from "@/components/Modal";
-import Spinner from "../../../components/Spiner";
+import Spinner from "@/components/Spiner";
+import { format, parseISO } from "date-fns";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface DisplayEditModalProps {
   isOpen: boolean;
@@ -22,8 +24,10 @@ const DisplayEditModal: React.FC<DisplayEditModalProps> = ({
 }) => {
   const supabase = createClient();
   const { words, setWords, fetchWords } = useUserStore();
-  const [isDeleteConfirmOpen, setIsDeleteConfirmOpen] = useState(false); 
-  const [ deleteLoading, setDeleteLoading] = useState(false)
+  const [isDeleteConfirmOpen, setIsDeleteConfirmOpen] = useState(false);
+  const [deleteLoading, setDeleteLoading] = useState(false);
+  const [showDetails, setShowDetails] = useState(false); 
+  const toggleDetails = () => setShowDetails((prev) => !prev);
 
   const handleSaveChanges = async () => {
     try {
@@ -42,6 +46,29 @@ const DisplayEditModal: React.FC<DisplayEditModalProps> = ({
     }
   };
 
+  //ブラウザ側でsupabaseを使う最もシンプルなパターン
+  // const handleDelete = async () => {
+  //   setDeleteLoading(true)
+  //   try {
+  //     const { error } = await supabase
+  //       .from("words")
+  //       .update({ deleted_at: new Date().toISOString() }) // deleted_atを更新
+  //       .eq("id", editWord!.id);
+
+  //     if (error) {
+  //       throw new Error("Error deleting word: " + error.message);
+  //     }
+
+  //     await fetchWords()
+  //     onClose(); // モーダルを閉じる
+  //   } catch (err: any) {
+  //     console.error(err.message);
+  //   } finally {
+  //     setDeleteLoading(false);
+  //   }
+  // };
+
+  //ブラウザ側でsupabaseを実行し、PromiseAllで最低0.5秒は待つパターン
   // const handleDelete = async () => {
   //   setDeleteLoading(true)
   //   try {
@@ -67,9 +94,9 @@ const DisplayEditModal: React.FC<DisplayEditModalProps> = ({
   //   }
   // };
 
-  
+  //APIを使うパターン（ちょっと遅い...?）
   const handleDelete = async () => {
-    setDeleteLoading(true)
+    setDeleteLoading(true);
 
     try {
       const response = await fetch("/api/deleteWord", {
@@ -79,13 +106,13 @@ const DisplayEditModal: React.FC<DisplayEditModalProps> = ({
         },
         body: JSON.stringify({ wordId: editWord!.id }),
       });
-      
+
       if (!response.ok) {
         throw new Error("Error deleting word");
       }
 
       await fetchWords();
-      onClose(); 
+      onClose();
     } catch (err: any) {
       console.error(err.message);
     } finally {
@@ -93,32 +120,24 @@ const DisplayEditModal: React.FC<DisplayEditModalProps> = ({
     }
   };
 
-  // const handleDelete = async () => {
-  //   setDeleteLoading(true)
-  //   try {
-  //     const { error } = await supabase
-  //       .from("words")
-  //       .update({ deleted_at: new Date().toISOString() }) // deleted_atを更新
-  //       .eq("id", editWord!.id);
-
-  //     if (error) {
-  //       throw new Error("Error deleting word: " + error.message);
-  //     }
-
-  //     await fetchWords()
-  //     onClose(); // モーダルを閉じる
-  //   } catch (err: any) {
-  //     console.error(err.message);
-  //   } finally {
-  //     setDeleteLoading(false);
-  //   }
-  // };
+  const formatDate = (dateString: string) => {
+    if (!dateString) return;
+    const parsedUTCDate = parseISO(dateString); //デフォルトでその土地の時間で計算してくれます
+    return format(parsedUTCDate, "yyyy年M月d日 H:m");
+  };
 
   return (
-   <Modal isOpen ={isOpen} onClose={onClose}>
-      <div className="relative flex items-center justify-center mt-2 mb-3">
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <button
+        onClick={onClose}
+        className="absolute top-3 right-3 text-gray-400 hover:text-gray-600 hover:bg-gray-100 rounded-full transition duration-200"
+      >
+        <XMarkIcon className="h-8 w-8 p-1" />
+      </button>
+
+      <div className="relative flex items-center justify-center mt-2 xs:mt-0 mb-3">
         <PencilSquareIcon className="h-5" />
-        <h2 className="font-bold text-xl ">カードを編集</h2>
+        <h2 className="font-bold text-xl ">単語カード</h2>
       </div>
 
       <div>
@@ -127,14 +146,13 @@ const DisplayEditModal: React.FC<DisplayEditModalProps> = ({
           <input
             type="text"
             value={editWord?.word}
-            onChange={(e) =>
-              setEditWord((prev) => prev && { ...prev, word: e.target.value })
-            }
+            onChange={(e) => setEditWord((prev) => prev && { ...prev, word: e.target.value })}
             className="
               w-full px-3 py-2 
               border rounded-lg 
               text-gray-800 font-semibold
-              focus:outline-none focus:border-gray-700 focus:border-1 transition-colors"
+              focus:outline-none focus:border-1 transition-colors 
+              focus:border-gray-700 "
           />
         </div>
 
@@ -143,9 +161,7 @@ const DisplayEditModal: React.FC<DisplayEditModalProps> = ({
           <input
             type="text"
             value={editWord?.meaning}
-            onChange={(e) =>
-              setEditWord((prev) => prev && { ...prev, meaning: e.target.value })
-            }
+            onChange={(e) => setEditWord((prev) => prev && { ...prev, meaning: e.target.value })}
             className="
               w-full px-3 py-2 
               border rounded-lg 
@@ -158,9 +174,7 @@ const DisplayEditModal: React.FC<DisplayEditModalProps> = ({
           <label className="text-gray-600 ml-1">例文</label>
           <textarea
             value={editWord?.example}
-            onChange={(e) =>
-              setEditWord((prev) => prev && { ...prev, example: e.target.value })
-            }
+            onChange={(e) => setEditWord((prev) => prev && { ...prev, example: e.target.value })}
             className="
               w-full px-3 py-2 
               border rounded-lg 
@@ -174,9 +188,7 @@ const DisplayEditModal: React.FC<DisplayEditModalProps> = ({
           <textarea
             value={editWord?.example_translation}
             onChange={(e) =>
-              setEditWord(
-                (prev) => prev && { ...prev, example_translation: e.target.value }
-              )
+              setEditWord((prev) => prev && { ...prev, example_translation: e.target.value })
             }
             className="
               w-full px-3 py-2 
@@ -190,9 +202,7 @@ const DisplayEditModal: React.FC<DisplayEditModalProps> = ({
           <label className="text-gray-600 ml-1">メモ</label>
           <textarea
             value={editWord?.memo}
-            onChange={(e) =>
-              setEditWord((prev) => prev && { ...prev, memo: e.target.value })
-            }
+            onChange={(e) => setEditWord((prev) => prev && { ...prev, memo: e.target.value })}
             className="
               w-full px-3 py-2 
               border rounded-lg 
@@ -200,8 +210,8 @@ const DisplayEditModal: React.FC<DisplayEditModalProps> = ({
               focus:outline-none focus:border-gray-700 focus:border-1 transition-colors"
           />
         </div>
-        <div className="mb-5">
-          <label className="text-gray-600 ml-1">優先度</label>
+        <div className="mb-3 px-0.5">
+          <label className="text-gray-600 ml-0.5">優先度</label>
           <CustomSlider
             sliderValue={editWord?.index || 0}
             onChange={(value) => setEditWord((prev) => prev && { ...prev, index: value })}
@@ -209,68 +219,136 @@ const DisplayEditModal: React.FC<DisplayEditModalProps> = ({
         </div>
       </div>
 
-      {/* ボタン */}
-      <div className="flex justify-center gap-3 my-10">
+    {/* 保存ボタン */}
+      {/* <div className="flex justify-center mb-8">
         <button
           onClick={handleSaveChanges}
-          className="w-full py-2 bg-blue-500 hover:bg-blue-600 text-white font-[550] rounded-lg transition duration-300"
+          className="w-full px-8  py-2 text-lg bg-blue-500 hover:bg-blue-600 text-gray-50 font-semibold rounded-xl transition duration-300"
         >
           変更を保存
         </button>
+      </div> */}
+
+      <div className="flex justify-start">
         <button
-          onClick={onClose}
-          className="w-full py-2  bg-gray-300 hover:bg-gray-400 text-black font-[550] rounded-lg transition duration-300"
+          type="button"
+          className=" rounded-lg text-gray-700  hover:text-gray-500 transition "
+          onClick={toggleDetails}
         >
-          閉じる
+          {showDetails 
+            ? (<div className="flex items-center justify-center gap-0.5"><ChevronDownIcon className="h-5"/>詳細情報</div>) 
+            : (<div className="flex items-center justify-center gap-0.5"><ChevronRightIcon className="h-5"/>詳細情報</div>)}
         </button>
       </div>
+    
+    {/* 詳細情報 */}
+    <AnimatePresence>
+      {showDetails && (
+        <motion.div
+          initial={{ height: 0, opacity: 0 }}
+          animate={{ height: "auto", opacity:1}}
+          exit={{ height: 0, opacity: 0 }}
+          transition={{ duration: 0.3 }}
+          className="overflow-hidden"
+        >
+          <div className="flex flex-col gap-3 ml-1 mt-3 mb-1 text-gray-600  ">
+        
+            <div className="flex">
+              <span className="w-[5.1rem]">復習回数</span>
+              <span className="mx-1">:</span>
+              <span className="ml-1 ">{editWord?.review_count}</span>
+            </div>
 
-      <div className="flex justify-center mt-4">
+            <div className="flex ">
+              <span className="w-[5.1rem]">最終復習日</span>
+              <span className="mx-1">:</span>
+              <span className="ml-1 ">
+                {editWord?.reviewed_at
+                  ? (formatDate(editWord?.created_at))
+                  : "未復習"
+                }
+              </span>
+            </div>
+
+            <div className="flex items-center">
+              <span className="w-[5.1rem]">作成日時</span>
+              <span className="mx-1">:</span>
+              <span className="ml-1 ">
+                {editWord?.created_at && formatDate(editWord?.created_at)}
+              </span>
+            </div>
+
+            <div className="flex items-center ">
+              <span className="w-[5.1rem]">最終更新日</span>
+              <span className="mx-1">:</span>
+              <span className="ml-1 ">
+                {editWord?.updated_at && formatDate(editWord?.updated_at)} <br />
+              </span>
+            </div>
+          </div>
+        </motion.div>)}
+    </AnimatePresence>
+
+      <div className="flex justify-between mb-1 mt-7">
         <button
-          onClick={() => setIsDeleteConfirmOpen(true)} 
+          onClick={() => setIsDeleteConfirmOpen(true)}
           className="
-            w-40 py-2 rounded-full text-sm -mb-1
-            hover:bg-gray-200 text-gray-600 font-[580] transition duration-300
+            px-4 py-1.5 rounded-lg  bg-gray-100 border
+            hover:bg-gray-300 text-gray-600 font-[580] transition duration-300
             flex items-center justify-center gap-0.5"
         >
-          <TrashIcon className="h-5"/>
-          単語を削除する
+          <TrashIcon className="h-4" />
+          削除
+        </button>
+
+        <button
+          onClick={handleSaveChanges}
+          className="px-6  py-1.5  bg-blue-500 hover:bg-blue-600 text-gray-50 font-[550] rounded-lg transition duration-300"
+        >
+          保 存
         </button>
       </div>
-
+      
+      {/* 削除確認モダール */}
       {isDeleteConfirmOpen && (
-          <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 w-full">
-            <div className="bg-white p-6 rounded-xl shadow-lg w-4/5 max-w-sm">
-              <h3 className="font-semibold mb-4 text-center">本当に削除しますか？</h3>
-              <div className="flex justify-center gap-3">
-                <button
-                  onClick={() => {
-                    handleDelete();
-                  }}
-                  className="
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 w-full">
+          <div className="bg-white p-6 rounded-xl shadow-lg w-4/5 max-w-sm">
+            <h3 className="font-semibold mb-4 text-center">本当に削除しますか？</h3>
+            <div className="flex justify-center gap-3">
+              <button
+                onClick={() => {
+                  handleDelete();
+                }}
+                className="
                     w-full py-2 
                     bg-gray-500 text-white font-[550] rounded-lg 
                     hover:bg-gray-600 transition duration-200
                     flex justify-center items-center gap-1"
-                >
-                  <span>削除</span>
-                  {deleteLoading 
-                    ? <Spinner size="h-5 w-5" borderColor="border-gray-100 border-t-gray-400" borderWeight="border-[3px]"/>
-                    : <TrashIcon className="h-5"/>}
-                </button>
-                <button
-                  onClick={() => setIsDeleteConfirmOpen(false)}
-                  className="
+              >
+                <span>削除</span>
+                {deleteLoading ? (
+                  <Spinner
+                    size="h-5 w-5"
+                    borderColor="border-gray-100 border-t-gray-400"
+                    borderWeight="border-[3px]"
+                  />
+                ) : (
+                  <TrashIcon className="h-5" />
+                )}
+              </button>
+              <button
+                onClick={() => setIsDeleteConfirmOpen(false)}
+                className="
                     w-full py-2 bg-gray-300 text-black rounded-lg font-[550
                     hover:bg-gray-200 transition duration-200]"
-                >
-                  キャンセル
-                </button>
-              </div>
+              >
+                キャンセル
+              </button>
             </div>
           </div>
-        )}
-  </Modal>
+        </div>
+      )}
+    </Modal>
   );
 };
 
