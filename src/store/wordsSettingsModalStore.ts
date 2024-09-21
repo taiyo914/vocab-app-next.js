dba45eb45249fd1627a8447f6b2736327a2c3605
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-21 18:35:35 +0900

    Rename file and make showDetails globally managable

---
+++ src/store/wordsSettingsModalStore.ts
@@ -0,0 +1,17 @@
+import { create } from 'zustand';
+
+type WordsSettingsModalState = {
+  isOpen: boolean;
+  toggleModal: () => void;
+  showDetails: boolean;
+  toggleDetails: () => void;
+};
+
+const useWordsSettingsModalStore = create<WordsSettingsModalState>((set) => ({
+  isOpen: false,
+  toggleModal: () => set((state) => ({ isOpen: !state.isOpen })),
+  showDetails: false,
+  toggleDetails: () => set((state) => ({showDetails: !state.showDetails}))
+}));
+
+export default useWordsSettingsModalStore;
