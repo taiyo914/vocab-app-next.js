26712be1a768d7b7222e2d2c8aa4b3aaf4338fff
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-22 09:20:47 +0900

    Implement the funtionality to show DisplayEditModal

--- src/app/display/displayContent/CardsItem.tsx
+++ src/app/display/displayContent/CardsItem.tsx
@@ -4,20 +4,29 @@ import { WordType } from "@/types/Types";
 import { motion } from "framer-motion";
 import ReactCardFlip from 'react-card-flip';
 import { PencilSquareIcon } from "@heroicons/react/24/outline";
+import DisplayEditModal from "./DisplayEditModal";
 
 const CardsItem = ({ word }:{ word: WordType}) => {
   const [isFlipped, setIsFlipped] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [editWord, setEditWord] = useState<WordType | null>(null);
+  const handleEditClick = ( event:any, word: WordType) => {
+    event.stopPropagation(); 
+    setEditWord(word);
+    setIsEditModalOpen(true);
+  };
 
-  const handleClick = (event:any) => {
+  const closeModal = () => {
+    setIsEditModalOpen(false);
+    setEditWord(null);
+  };
+
+  const handleClick = (event:React.MouseEvent<HTMLElement>) => {
     event.stopPropagation(); // クリックイベントの伝播を防止
     setIsFlipped(!isFlipped);
   };
 
-  const buttonClick = (event:any) =>{
-    event.stopPropagation(); // クリックイベントの伝播を防止
-    alert("クリックされました")
-  }
-  return (
+  return (<>
     <motion.div
       layout
       whileHover={{ scale: 1.03 }}
@@ -45,13 +54,20 @@ const CardsItem = ({ word }:{ word: WordType}) => {
             onClick={handleClick} 
             key="back"
           >
-            <button onClick={buttonClick} className="absolute top-2 right-2 text-gray-400 hover:text-gray-600 transition duration-200"><PencilSquareIcon className="h-5"/></button>
+            <button onClick={(e)=>handleEditClick(e, word)} className="absolute top-2 right-2 text-gray-400 hover:text-gray-600 transition duration-200"><PencilSquareIcon className="h-5"/></button>
             <h2 className="text-[1.2rem] font-semibold text-center bg-white">{word.meaning}</h2>
           </div>
         
       </ReactCardFlip>
     </motion.div>
-  );
+
+    <DisplayEditModal
+      isOpen={isEditModalOpen}
+      onClose={closeModal}
+      editWord={editWord}
+      setEditWord={setEditWord}
+    />
+  </>);
 };
 
 export default CardsItem;
