122e9655d6c1652da28efff4909f1042351e7630
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-22 14:19:23 +0900

    Ensure the updated date is also reflected

--- src/app/display/displayContent/DisplayEditModal.tsx
+++ src/app/display/displayContent/DisplayEditModal.tsx
@@ -33,12 +33,18 @@ const DisplayEditModal: React.FC<DisplayEditModalProps> = ({
 
   const handleSaveChanges = async () => {
     try {
+
+      const updatedAt = new Date().toISOString(); 
+      const updatedWord = { ...editWord, updated_at: updatedAt }; 
+
       const { error } = await supabase.from("words").update(editWord).eq("id", editWord!.id);
 
       if (error) throw new Error(error.message);
       else console.log("変更に成功しました！");
 
-      const updateWords = words!.map((word) => (word.id === editWord!.id ? { ...editWord } : word));
+      const updateWords = words!.map((word) => 
+        word.id === editWord!.id ? updatedWord : word
+      );
 
       setWords(updateWords as WordType[]);
 
@@ -87,10 +93,10 @@ const DisplayEditModal: React.FC<DisplayEditModalProps> = ({
   return (
     <Modal isOpen={isOpen} onClose={onClose} >
 
-        <div className="flex items-center justify-center mt-3 xs:mt-0 mb-4">
-          <PencilSquareIcon className="h-5" />
-          <h2 className="font-bold text-xl ">単語カード</h2>
-        </div>
+      <div className="flex items-center justify-center mt-3 xs:mt-0 mb-4">
+        <PencilSquareIcon className="h-5" />
+        <h2 className="font-bold text-xl ">単語カード</h2>
+      </div>
 
       <div>
         <div className="mb-5">
@@ -247,101 +253,83 @@ const DisplayEditModal: React.FC<DisplayEditModalProps> = ({
       </div>
     
     {/* 詳細情報 */}
-    <AnimatePresence>
-      {showDetails && (
-        <motion.div
-          initial={{ height: 0, opacity: 0 }}
-          animate={{ height: "auto", opacity:1}}
-          exit={{ height: 0, opacity: 0 }}
-          transition={{ duration: 0.3 }}
-          className="overflow-hidden"
-        >
-          <div className="flex flex-col gap-3 ml-1 mt-4 mb-1 text-gray-600  ">
-        
-            <div className="flex">
-              <span className="w-[5.1rem]">復習回数</span>
-              <span className="mx-1">:</span>
-              <span className="ml-1 ">{editWord?.review_count}</span>
+      <AnimatePresence>
+        {showDetails && (
+          <motion.div
+            initial={{ height: 0, opacity: 0 }}
+            animate={{ height: "auto", opacity:1}}
+            exit={{ height: 0, opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            className="overflow-hidden"
+          >
+            <div className="flex flex-col gap-3 ml-1 mt-4 mb-1 text-gray-600  ">
+          
+              <div className="flex">
+                <span className="w-[5.1rem]">復習回数</span>
+                <span className="mx-1">:</span>
+                <span className="ml-1 ">{editWord?.review_count}</span>
+              </div>
+
+              <div className="flex ">
+                <span className="w-[5.1rem]">最終復習日</span>
+                <span className="mx-1">:</span>
+                <span className="ml-1 ">
+                  {editWord?.reviewed_at
+                    ? (formatDate(editWord?.reviewed_at))
+                    : "未復習"
+                  }
+                </span>
+              </div>
+
+              <div className="flex items-center">
+                <span className="w-[5.1rem]">作成日時</span>
+                <span className="mx-1">:</span>
+                <span className="ml-1 ">
+                  {editWord?.created_at && formatDate(editWord?.created_at)}
+                </span>
+              </div>
+
+              <div className="flex items-center ">
+                <span className="w-[5.1rem]">最終更新日</span>
+                <span className="mx-1">:</span>
+                <span className="ml-1 ">
+                  {editWord?.updated_at && formatDate(editWord?.updated_at)} <br />
+                </span>
+              </div>
             </div>
+          </motion.div>)}
+      </AnimatePresence>
 
-            <div className="flex ">
-              <span className="w-[5.1rem]">最終復習日</span>
-              <span className="mx-1">:</span>
-              <span className="ml-1 ">
-                {editWord?.reviewed_at
-                  ? (formatDate(editWord?.created_at))
-                  : "未復習"
-                }
-              </span>
-            </div>
-
-            <div className="flex items-center">
-              <span className="w-[5.1rem]">作成日時</span>
-              <span className="mx-1">:</span>
-              <span className="ml-1 ">
-                {editWord?.created_at && formatDate(editWord?.created_at)}
-              </span>
-            </div>
-
-            <div className="flex items-center ">
-              <span className="w-[5.1rem]">最終更新日</span>
-              <span className="mx-1">:</span>
-              <span className="ml-1 ">
-                {editWord?.updated_at && formatDate(editWord?.updated_at)} <br />
-              </span>
-            </div>
-          </div>
-        </motion.div>)}
-    </AnimatePresence>
-
-      {/* <div className="flex justify-between mt-7 xs:-mb-1">
-        <button
-          onClick={() => setIsDeleteConfirmOpen(true)}
-          className="
-            px-4 py-1.5 rounded-lg  bg-gray-100 border
-            hover:bg-gray-300 text-gray-600 font-[580] transition duration-300
-            flex items-center justify-center gap-0.5"
-        >
-          <TrashIcon className="h-4" />
-          削除
-        </button>
-
+    {/* ボタン */}
+      <div className="flex gap-3 mt-6 xs:-mb-1">
         <button
           onClick={handleSaveChanges}
-          className="px-6  py-1.5  bg-blue-500 hover:bg-blue-600 text-white font-[550] rounded-lg transition duration-300"
+          className="py-2 border rounded-xl text-white w-full bg-blue-500 hover:bg-blue-600 transition-colors duration-300 font-semibold"
         >
           保 存
         </button>
-      </div> */}
+        <button
+          onClick = {onClose} 
+          className="cursor-pointer text-center py-2 border rounded-xl text-white w-full bg-gray-500 hover:bg-gray-600 transition-colors duration-300 font-semibold" 
+        >
+          閉じる
+        </button>
+      </div>
 
-        <div className="flex gap-3 mt-6 xs:-mb-1">
-          <button
-            onClick={handleSaveChanges}
-            className="py-2 border rounded-xl text-white w-full bg-blue-500 hover:bg-blue-600 transition-colors duration-300 font-semibold"
-          >
-            保 存
-          </button>
-          <button
-            onClick = {onClose} 
-            className="cursor-pointer text-center py-2 border rounded-xl text-white w-full bg-gray-500 hover:bg-gray-600 transition-colors duration-300 font-semibold" 
-          >
-            閉じる
-          </button>
-        </div>
-
-        <div className="mt-7 flex justify-center ">
-          <button
-            onClick={() => setIsDeleteConfirmOpen(true)}
-            className="
-              w-40 py-2 rounded-full text-sm -mb-1
-            hover:bg-gray-200 text-gray-600 font-medium transition duration-300
-              flex items-center justify-center gap-0.5"
-          >
-            <TrashIcon className="h-4" />
-            単語を削除する
-          </button>
-        </div>
-      
+    {/* 削除ボタン */}
+      <div className="mt-7 flex justify-center ">
+        <button
+          onClick={() => setIsDeleteConfirmOpen(true)}
+          className="
+            w-40 py-2 rounded-full text-sm -mb-1
+          hover:bg-gray-200 text-gray-600 font-medium transition duration-300
+            flex items-center justify-center gap-0.5"
+        >
+          <TrashIcon className="h-4" />
+          単語を削除する
+        </button>
+      </div>
+  
       {/* 削除確認モダール */}
       {isDeleteConfirmOpen && (
         <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 w-full">
