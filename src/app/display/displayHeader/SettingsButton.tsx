b0ac938575072a04e1222bea706e6937fc4f7d83
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-23 22:28:08 +0900

    Pattern of vanishing button

--- src/app/display/displayHeader/SettingsButton.tsx
+++ src/app/display/displayHeader/SettingsButton.tsx
@@ -4,25 +4,20 @@ import useSearchStore from "@/store/searchStore";
 const SettingsButton = () => {
   const { toggleModal } = useWordsSettingsModalStore();
   const { searchTriggered } = useSearchStore();
-  const handleClick = () =>{
-    if( searchTriggered ){
-      alert("検索結果を表示している間は設定を開くことはできません...[ただいま改良中]")
-    } else {
-      toggleModal()
-    }
-  }
 
-  return (
-    <div
-      onClick={handleClick}
-      className={` ${searchTriggered ? "cursor-not-allowed" :"hover:bg-gray-100 cursor-pointer" }
-        flex items-center text-gray-500 
-        duration-300 rounded-lg p-1 px-2`}
-    >
-      <AdjustmentsHorizontalIcon className="h-6 text-gray-400 " />
-      設定
-    </div>
-  );
+  return (<>
+    {!searchTriggered && (
+      <button
+        onClick={toggleModal}
+        className={`hover:bg-gray-100 
+          flex items-center text-gray-500 
+          duration-300 rounded-lg p-1 px-2`}
+      >
+        <AdjustmentsHorizontalIcon className="h-6 text-gray-400 " />
+        設定
+      </button>
+    )}
+  </>);
 };
 
 export default SettingsButton;
