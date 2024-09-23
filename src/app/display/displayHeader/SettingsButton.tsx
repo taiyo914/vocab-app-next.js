24c804d03549850bee6607ef266fa91b815aa977
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-23 22:08:47 +0900

    Simplest way to avoid to open settings while showing search result

--- src/app/display/displayHeader/SettingsButton.tsx
+++ src/app/display/displayHeader/SettingsButton.tsx
@@ -1,16 +1,27 @@
 import { AdjustmentsHorizontalIcon } from "@heroicons/react/24/outline";
 import useWordsSettingsModalStore from "@/store/wordsSettingsModalStore";
+import useSearchStore from "@/store/searchStore";
 const SettingsButton = () => {
   const { toggleModal } = useWordsSettingsModalStore();
+  const { searchTriggered } = useSearchStore();
+  const handleClick = () =>{
+    if( searchTriggered ){
+      alert("検索結果を表示している間は設定を開くことはできません...[ただいま改良中]")
+    } else {
+      toggleModal()
+    }
+  }
 
   return (
-    <button
-      onClick={toggleModal}
-      className="cursor-pointer flex items-center text-gray-500 hover:bg-gray-100 duration-300 rounded-lg p-1 px-2"
+    <div
+      onClick={handleClick}
+      className={` ${searchTriggered ? "cursor-not-allowed" :"hover:bg-gray-100 cursor-pointer" }
+        flex items-center text-gray-500 
+        duration-300 rounded-lg p-1 px-2`}
     >
       <AdjustmentsHorizontalIcon className="h-6 text-gray-400 " />
       設定
-    </button>
+    </div>
   );
 };
 
