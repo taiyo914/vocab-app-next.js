dc67e3ac9d34edbfed943359aa33be1a114bde37
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-25 00:34:35 +0900

    Delete unnecessary discription

--- src/app/display/displayHeader/MobileSearchInput.tsx
+++ src/app/display/displayHeader/MobileSearchInput.tsx
@@ -5,33 +5,18 @@ import { useState, useEffect, useRef } from "react";
 import useSearchStore from "@/store/searchStore";
 import { WordType } from "@/types/Types";
 import EditWordModal from "@/components/EditWordModal";
-import useUserStore from "@/store/userStore";
 let lastRequestTime = 0;
 
 const MobileSearchInput = () => {
   const [inputValue, setInputValue] = useState("");
-  const {
-    tempResults,
-    setTempResults,
-    setResults,
-    searchTriggered,
-    setSearchTriggered,
-    isOpen,
-    setIsOpen,
-  } = useSearchStore();
-  const incrementfechingKey = useUserStore((state) => state.incrementFetchingKey);
+  const {tempResults, setTempResults, isOpen, setIsOpen,} = useSearchStore();
   const [isFirstSearch, setIsFirstSearch] = useState(true);
-  const [showResults, setShowResults] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isBackgroundDimmed, setIsBackgroundDimmed] = useState(false); // 背景暗くするフラグ
   const [selectedWord, setSelectedWord] = useState<WordType | null>(null);
   const handleWordClick = (word: WordType) => {
     setSelectedWord(word);
     setIsModalOpen(true);
   };
-
-  const inputRef = useRef<HTMLInputElement | null>(null);
-  const containerRef = useRef<HTMLDivElement | null>(null);
 
   //後ろの画面をスクロールできなくする設定
   useEffect(() => {
@@ -45,64 +30,10 @@ const MobileSearchInput = () => {
     };
   }, [isOpen]);
 
-  const clickSearchIcon = () => {
-    if (!inputValue.trim()) {
-      setIsOpen(!isOpen);
-      setSearchTriggered(false);
-      setIsBackgroundDimmed(!isOpen);
-    }
-
-    if (!isOpen) {
-      setTimeout(() => {
-        inputRef.current?.focus();
-        setShowResults(true);
-      }, 500);
-    }
-
-    if (inputValue.trim()) {
-      setResults(tempResults);
-      setSearchTriggered(true);
-      setShowResults(false);
-      incrementfechingKey(); //keyを変更してアニメーションを発火
-    }
-  };
-
   const handleClear = () => {
     setInputValue("");
-    setResults([]);
     setIsFirstSearch(true);
     setIsOpen(false);
-    setShowResults(false);
-    if (searchTriggered) incrementfechingKey(); //keyを変更してアニメーションを発火
-    setSearchTriggered(false);
-    setIsBackgroundDimmed(false);
-  };
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (!isOpen) return; //検索欄が開いているときだけ有効にする
-
-      if (containerRef.current && !containerRef.current.contains(event.target as Node)) {
-        if (inputValue.trim() === "") {
-          handleClear();
-        } else {
-          setShowResults(false);
-        }
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [inputValue, searchTriggered, isOpen]);
-
-  // 入力欄にフォーカスがあたったら検索結果を再表示する
-  const handleFocus = () => {
-    if (inputValue) {
-      setShowResults(true);
-    }
   };
 
   const fetchResults = async (searchTerm: string) => {
@@ -127,37 +58,35 @@ const MobileSearchInput = () => {
   useEffect(() => {
     if (inputValue) {
       fetchResults(inputValue);
-      setShowResults(true);
     } else {
       setTempResults([]);
       setIsFirstSearch(true);
-      setShowResults(false);
     }
   }, [inputValue]);
 
   return (<>
   {/* 背景を暗くする */}
-    {isBackgroundDimmed && (
+    {isOpen && (
       <motion.div
       initial = {{opacity:0,}}
       animate = {{opacity:1}}
       transition={{duration:0.5, ease: "easeInOut"}}
         onClick={() => {
           setIsOpen(false);
-          setIsBackgroundDimmed(false); // 背景を元に戻す
         }}
         className="fixed inset-0 bg-black bg-opacity-20 z-10"
       />
     )}
   
-     <div ref={containerRef} className="" >
+     <div className="" >
        {/* 画面右下に固定された検索アイコン */}
-       <button
-         onClick={clickSearchIcon}
-         className="fixed bottom-4 right-4 bg-gray-100 p-2 rounded-full shadow-lg z-10"
-       >
-         <MagnifyingGlassIcon className="h-[24px] text-gray-400 cursor-pointer" />
-       </button>
+       <div
+        onClick = {() => setIsOpen(true)}
+        className={`flex items-center hover:bg-gray-100 duration-300 rounded-lg py-1`}
+      >
+        <MagnifyingGlassIcon className="h-[22px] text-gray-400 cursor-pointer" />
+        <span className="text-gray-500 transition-all">検索</span>
+      </div>
 
        {/* 検索モーダル部分 */}
        <motion.div
@@ -168,25 +97,27 @@ const MobileSearchInput = () => {
          className={`fixed top-20 left-0 w-full bottom-0 bg-white z-10 ${ isOpen && "shadow-lg"} `}
        >
           <div className="mt-3 flex flex-col h-full">
-           <div className="flex items-center justify-between gap-2 px-2 bg-white">
-             <button onClick={clickSearchIcon}>
-               <MagnifyingGlassIcon className="h-[24px]  text-gray-400 cursor-pointer" />
-             </button>
-             <input
-               ref={inputRef}
-               type="text"
-               value={inputValue}
-               onChange={(e) => setInputValue(e.target.value)}
-               className="w-full py-2 px-4 focus:outline-none border rounded-md"
-               placeholder="検索キーワードを入力..."
-             />
-             <button onClick={handleClear} className="">
-               <XCircleIcon className="h-[24px] text-gray-500" />
-             </button>
+            <div className="">
+              <div className="flex items-center justify-between gap-2 px-2 bg-white">
+                <MagnifyingGlassIcon className="h-[24px]   text-gray-400" />
+                <input
+                  id = "input"
+                  type="text"
+                  value={inputValue}
+                  onChange={(e) => setInputValue(e.target.value)}
+                  className="w-full py-2 px-3 focus:outline-none border rounded-md"
+                  placeholder="検索キーワードを入力..."
+                  autoFocus={true}
+                  tabIndex={0}
+                />
+                <button onClick={handleClear} className="">
+                  <XCircleIcon className="h-[24px] text-gray-500 " />
+                </button>
+              </div>
            </div>
            {/* 検索結果の表示 */}
-           <div className="mt-4 overflow-y-auto flex-grow">
-             {showResults && inputValue && tempResults.length > 0 && (
+           <div className="mt-3 overflow-y-auto flex-grow">
+             {inputValue && tempResults.length > 0 && (
                <ul className="max-h-full overflow-y-auto">
                  {tempResults.map((word: WordType) => (
                    <li
@@ -202,7 +133,7 @@ const MobileSearchInput = () => {
              )}
 
              {/* 結果がない場合 */}
-             {showResults && !isFirstSearch && inputValue && tempResults.length === 0 && (
+             {!isFirstSearch && inputValue && tempResults.length === 0 && (
                <ul className="p-2 text-gray-400 mt-4">
                  <li>結果なし</li>
                </ul>
@@ -226,14 +157,4 @@ const MobileSearchInput = () => {
 
 export default MobileSearchInput;
 
-// useEffect(() => {
-//   if (isOpen) {
-//     document.body.style.overflow = "hidden";
-//   } else {
-//     document.body.style.overflow = "";
-//   }
-//   return () => {
-//     document.body.style.overflow = "";
-//   };
-// }, [isOpen]);
 
