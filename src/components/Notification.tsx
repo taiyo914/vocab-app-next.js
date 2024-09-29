8a040526a03517a3b4bb4b5d09105d3051634e7d
taiyo914 <taiyosuzuki0914@icloud.com>
2024-09-29 10:19:29 +0900

    Add backgroundColor. See #1

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -5,7 +5,7 @@ import { XMarkIcon } from "@heroicons/react/24/outline";
 import useNotificationStore from "@/store/useNotificationStore";
 
 const Notification = () => {
-  const { show, message, duration, hideNotification } = useNotificationStore();
+  const { show, message, duration, hideNotification, backgroundColor } = useNotificationStore();
 
   useEffect(() => {
     if (show) {
@@ -25,7 +25,8 @@ const Notification = () => {
           exit={{ opacity: 0, x: "100%" }} 
           transition={{type: "spring", stiffness: 240, damping: 30}}
           // transition ={{duration: 0.3}}
-          className="fixed top-4 right-4 rounded-lg shadow-lg border bg-white z-100"
+          className="fixed top-4 right-4 rounded-xl shadow-lg border z-100"
+          style={{ backgroundColor }}
         >
           <div className="relative py-[25px] pr-[30px] pl-[30px] max-w-[350px]">
             <span>{message}</span>
--- src/store/useNotificationStore.ts
+++ src/store/useNotificationStore.ts
@@ -4,7 +4,8 @@ interface NotificationState {
   show: boolean;
   message: string;
   duration: number;
-  showNotification: (message: string, duration? :number) => void;
+  backgroundColor: string; 
+  showNotification: (message: string, duration? :number, backgroundColor?: "green" | "red" | "blue") => void;
   hideNotification: () => void;
 }
 
@@ -12,8 +13,16 @@ const useNotificationStore = create<NotificationState>((set) => ({
   show: false,
   message: "",
   duration: 3000,
-  showNotification: (message: string, duration: number = 3000) => set({ show: true, message, duration }),
-  hideNotification: () => set({ show: false, message: "", duration: 3000 }),
+  backgroundColor: "white", 
+  showNotification: (message: string, duration: number = 4000,  backgroundColor:string = "white") => {
+    let bgColor = "white"; // デフォルトの色
+    if (backgroundColor === "green") bgColor = "#d8f0dd";
+    if (backgroundColor === "red") bgColor = "#ffdcdf";
+    if (backgroundColor === "blue") bgColor = "#e0eafc";
+    
+    set({ show: true, message, duration, backgroundColor: bgColor })
+  },
+  hideNotification: () => set({ show: false, message: "", duration: 4000,  backgroundColor: "white" }),
 }));
 
 export default useNotificationStore;
