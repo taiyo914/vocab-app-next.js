78cf2fbc596d97085e3f5fc45aa0429ee1909b8e
taiyo914 <taiyosuzuki0914@icloud.com>
2024-10-04 00:41:08 +0900

    Delete unnecessary description

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,17 +1,9 @@
-"use client";
-
-import { motion } from "framer-motion";
-import {
-  ArrowRightIcon,
-  
-} from "@heroicons/react/24/outline";
-import Link from "next/link";
 import HeroSection from "./HeroSection";
 import FeaturesSection from "./FeaturesSection";
 import GetStartedSection from "./GetStartedSection";
 import AboutSection from "./AboutSection";
 
-export default function Component() {
+export default function Landing() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50 overflow-hidden">
       <main className="flex-1 pt-16">
