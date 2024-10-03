bc4c9cff04ce85e5dea55e1d5353a7d4dcb3b358
taiyo914 <taiyosuzuki0914@icloud.com>
2024-10-03 23:22:58 +0900

    Change the rounting to /landing

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -38,11 +38,12 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/signin') &&
-    !request.nextUrl.pathname.startsWith('/signup')
+    !request.nextUrl.pathname.startsWith('/signup') &&
+    !request.nextUrl.pathname.startsWith('/landing')
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
-    url.pathname = '/signin'
+    url.pathname = '/landing'
     return NextResponse.redirect(url)
   }
 
